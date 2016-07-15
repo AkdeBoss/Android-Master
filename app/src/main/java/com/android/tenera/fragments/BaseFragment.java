75c408b700e0d8b6697ee0d52929f95fc1f44a82
kallubandi <raghavendra.kallubandi@gmail.com>
2016-07-16 01:04:50 +0530

    Network Manager Added

--- app/src/main/java/com/android/tenera/fragments/BaseFragment.java
+++ app/src/main/java/com/android/tenera/fragments/BaseFragment.java
@@ -4,5 +4,9 @@ import android.support.v4.app.Fragment;
 
 
 public class BaseFragment extends Fragment {
+    public interface NetworkCallListener {
+        void onSuccess(Object response);
 
+        void onFailure(Throwable error);
+    }
 }
